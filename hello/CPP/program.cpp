21bc76ae590a7570acf06fbb914ae6538ceda741
Rasika Weliwita <weliwita@gmail.com>
2017-02-02 05:18:27 +0530

    hello cpp

---
+++ hello/CPP/program.cpp
@@ -0,0 +1,11 @@
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    char ch = 'A';
+    short sh = ch;
+    //printf("peak point is %d", elem);
+    cout << sh << endl;
+    return 0;
+}
