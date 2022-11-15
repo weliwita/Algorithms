cea83e8844c28a79228d177001e4edab96481bef
Rasika Weliwita <rasika@creativesoftware.com>
2022-11-16 04:48:23 +0530

    initial c

---
+++ competitiveprogrammingbook/setup/program.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+    //make input output more efficient.
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int a, b;
+    string x;
+    cin >> a >> b >> x;
+    cout << a << " " << b << " " << x << "\n";
+}
